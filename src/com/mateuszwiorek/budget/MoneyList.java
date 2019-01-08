f4349e273b53fbda8cc7d9c2ebb0aa99e19b800f
MateuszWiorek <mateusz.wiorek@onet.pl>
2019-01-08 13:42:18 +0100

    Change names

--- src/com/mateuszwiorek/budget/MoneyList.java
+++ src/com/mateuszwiorek/budget/MoneyList.java
@@ -7,12 +7,6 @@ import java.util.List;
 public class MoneyList {
 
     public List<Money> moneyList = new ArrayList<Money>();
-    public List<Money> getMoneyList() {
-        return moneyList;
-    }
-    public void setmoneyList(List<Money> moneyList){
-        this.moneyList=moneyList;
-    }
 
     public void addtToMoneyList(Money money){
         this.moneyList.add(money);
--- src/com/mateuszwiorek/budget/UsersList.java
+++ src/com/mateuszwiorek/budget/UsersList.java
@@ -2,18 +2,14 @@ package com.mateuszwiorek.budget;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * klasa zawierająca listę obieków  typu Users
- */
+
 public class UsersList {
-    Users user1= new Users("Paweł", "lubieplacki123");
-    Users user2= new Users( "Mateusz", "12345");
-    Users user3= new Users("Filip", "Ewe123");
-    Users user4= new Users("Ewelina", "ja15");
+    Users user1= new Users("Pablo", "lubieplacki123");
+    Users user2= new Users( "Matthew", "12345");
+    Users user3= new Users("Kate", "Ewe123");
+    Users user4= new Users("Marcela", "ja15");
     public  List<Users> usersList = new LinkedList<Users>();
-    /**
-     * konstruktor UsersList tworzy listę użytkowników, których używamy w aplikacji
-     */
+
     UsersList(){
         usersList.add(user1);
         usersList.add(user2);
